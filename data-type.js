b1d2d22b4dc0f35015893d26f52960eee9878392
Rahul Pathak <137425116+Rahul7966@users.noreply.github.com>
2024-01-07 14:35:12 +0000

    Discussion about data type

---
+++ data-type.js
@@ -0,0 +1,47 @@
+// 2 data type in java-script 
+//primitive (STACK)
+/*
+String 
+Number
+Boolean : - True or False 
+Null    :- nothing
+Undefinded :- is also nothing or empty
+Symbol   :-  unique
+Big Int  :- large number
+
+*/
+//Example:- 
+const firstName = "Rahul Pathak"
+const age = 20
+const score = 200.5
+let isLoggedIn = true
+const nodeVersion = null
+let bankBalance = undefined
+const buttonId = Symbol("id")
+const aReallyLongNumber = 56585656666666655n
+
+//reference (non-primitive):- Objects (HEAP)
+
+/*
+Arrays
+object
+Function 
+
+*/
+
+//declaration of  array , onject , function
+const Array = ["Rahul", "Pathak", 123 , True ]
+const object = {Name  : "Rahul", age:123 , isLoggedIn :Ture}
+//function 
+
+const sayHello = function(){
+    console.log("Rahul Pathak")
+}
+
+const registerUser = function(){
+
+}
+
+const loginUser = function(){
+
+}
