f76ca166b52a74b0d9ba753ec6579650ea9b7c64
Rahul Pathak <137425116+Rahul7966@users.noreply.github.com>
2024-01-26 07:39:49 +0000

    qs2

---
+++ JavaScript-Assignment-4th_nov/QuestionNo2.js
@@ -0,0 +1,74 @@
+/* Q2. Describe the categorization of operators in JavaScript based on their functionality. Provide examples for
+each category.
+
+Solution:- 
+
+Operators in JavaScript are cateorized based on their functionality into the followin roups:
+
+1.arithmetic Operators: These operators perform mathematical operations on operands. Operands can be
+numbers or variables. /** */
+
+ //addition(+)
+ const sum = 1+2
+ console.log("addition of two number :", sum)
+ //subtraction(-)
+ const sub = 25-9
+ console.log("Subtraction of two number ", sub)
+ //Multipliction (*)
+ const mul = 2*8
+ console.log("Multiplication of two number ", mul)
+ //division (/)
+ const div = 8/2
+console.log("Division of tow number ", div)
+//Exponentiation(**)
+const power = 2**3
+console.log("power of the number")
+//Modulo(%)
+const mod = 54%14
+console.log("modulo of two number  ", mod)
+
+
+//2. Assignment  Operators: These operators assin values to variables 
+// Assignment operator (=)
+const myVariable = 10;
+// Addition assignment operator (+=)
+myVariable += 5; // myVariable is now equal to 15
+// Subtraction assignment operator (-=)
+myVariable -= 5; // myVariable is now equal to 10
+// Multiplication assignment operator (*=)
+myVariable *= 2; // myVariable is now equal to 20
+// Division assignment operator (/=)
+myVariable /= 2; // myVariable is now equal to 10
+// Modulo assignment operator (%=)
+myVariable %= 3; // myVariable is now equal to 1
+
+//3. Comparison Operators: These operators compare two values and return a Boolean value (true or false).
+
+// Equal to (==)
+const isEqual = 10 == 10; // isEqual is now equal to true
+// Not equal to (!=)
+const isNotEqual = 10 != 10; // isNotEqual is now equal to false
+// Greater than (>)
+const isGreaterThan = 10 > 5; // isGreaterThan is now equal to true
+// Less than (<)
+const isLessThan = 10 < 5; // isLessThan is now equal to false
+// Greater than or equal to (>=)
+const isGreaterThanOrEqual = 10 >= 10; // isGreaterThanOrEqual is now equal to
+true
+// Less than or equal to (<=)
+const isLessThanOrEqual = 10 <= 5; // isLessThanOrEqual is now equal to false
+
+//4. Logical Operators: These operators perform logical operations on Boolean values
+// AND (&&)
+const isAndTrue = true && true; // isAndTrue is now equal to true
+const isAndFalse = true && false; // isAndFalse is now equal to false
+// OR (||)
+const isOrTrue = true || false; // isOrTrue is now equal to true
+const isOrFalse = false || false; // isOrFalse is now equal to false
+// NOT (!)
+const isNotTrue = !true; // isNotTrue is now equal to false
+const isNotFalse = !false; // isNotFalse is now equal to true
+
+
+
+ 
