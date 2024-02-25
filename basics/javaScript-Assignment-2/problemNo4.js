64fd6de39e4035c8de608f51d6cbdb65797009e4
Rahul Pathak <137425116+Rahul7966@users.noreply.github.com>
2024-02-25 07:28:58 +0000

    assigment 2

---
+++ basics/javaScript-Assignment-2/problemNo4.js
@@ -0,0 +1,9 @@
+/*Use a ternary operator to check that a person is eligible to vote or not by checking his age. If the age of the
+person is less than 18 then “You cannot vote” should be logged else “You can vote” should be logged./** */
+const age = 18;
+if(age >= 18){
+    console.log("you can vote ")
+}
+else{
+    console.log("you cannot vote")
+}
