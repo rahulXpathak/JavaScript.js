c968c91f3c8664f8bc7052eb4500e9424812ca03
Rahul Pathak <137425116+Rahul7966@users.noreply.github.com>
2024-02-25 17:43:31 +0000

    web

--- basics/javaScript-Assignment-2/problemNo3.js
+++ basics/javaScript-Assignment-2/problemNo3.js
@@ -1,12 +1,8 @@
 /*Use a nested ternary operator to check that a number is positive, negative or zero. You have to print
 “positive” if the number is positive and similarly for negative and zero also./** */
 const number = 0
-if (number > 0){
-    console.log(`This number:${number} is Positive `)
-}
-else if(number < 0 ){
-    console.log(`This number:${number} is negative .`)
-}
-else{
-    console.log(`This number is : ${number}`)
-}
+number > 0
+?console.log("positive")
+:number < 0
+?console.log("negative ")
+:console.log("zero")
