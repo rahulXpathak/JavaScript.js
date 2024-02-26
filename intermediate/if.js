766b1bb17504a08934eeed781647870701b4baf0
Rahul Pathak <137425116+Rahul7966@users.noreply.github.com>
2024-02-26 14:27:11 +0000

    oooo

--- intermediate/if.js
+++ intermediate/if.js
@@ -17,3 +17,60 @@ else{
 }
 
 console.log(`your score is ${score}`);
+
+
+
+
+//login System 
+const gmail = "rahulpathak@rp.in"
+
+// if email is present , ask for password , otherwis ask for email 
+if(gmail){
+    console.log("enter your password ")
+
+}else{
+    console.log('Please enter your gmail again :')
+}
+/*if(gmail != ""){
+    console.log("Please provide a password")
+
+}else{
+    console.log("Please enter your Gamil Again");
+}/** */
+console.log(Boolean(gmail))
+
+
+
+// if user provides email , password , then allow login , els eask for it
+
+const email = ""
+const password = ""
+if(email){
+    if(password){
+        console.log("well to the app");
+    }
+}else{
+    console.log("please enter your email");
+}
+
+//logical operation 
+// && -> and ; || => OR 
+
+if(email && password){
+    console.log("wellcome to pw app")
+}else{
+    console.log("please enter your email and password");
+}
+
+
+
+
+
+// allow user to singin with google and github 
+const googleLoginToken = "" 
+const githubloginToken = ""
+if(googleLoginToken || githubloginToken){
+    console.log("wellcome to the app")
+}else{
+    console.log("please login to view your account ")
+}
